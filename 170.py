2acabcc7f22c93357f4cce033abe3aac2e28573d
Git-nirmay <79860151+Git-nirmay@users.noreply.github.com>
2022-04-16 02:58:38 +0530

    Add files via upload

---
+++ 170.py
@@ -0,0 +1,37 @@
+class Story_Of_Naruto():
+    def year1(self):
+        print("\n","Story Of Naruto","\n")
+        print("The timeline jumps forward several years between the end of Naruto and the beginning of Shippuden. The series begins with an older Naruto returning to the academy and we learn the progress of his friends. The trio at the center of the series are all training under legendary ninjas in individualized programs. Sakura has become an incredible healer, Naruto's abilities are much improved, and Sasuke is still on the run under the tutelage of the disgraced shinobi Orochimaru.The new series deals with the older ninjas fighting against the growing forces of the Akatsuki, who stage bolder and bolder attacks on various cities as cover to attempt to steal the sealed beast powers that a small class of fighters like Naruto possess.At the same time, the series offers a resolution to Sasuke's years-long quest to exact vengeance on his brother. Sasuke finds his brother and they engage in a long, drawn-out battle that features several feints and mind projections. Using their illusory ninjitsu abilities, the brothers go through several battles that end in gruesome and psychedelic ways, only for it to be revealed that the battle has yet to start outside of their minds.The pair eventually do fall into a pitched battle, blasting each other with ever-increasing attacks. Sasuke notices that his brother is off, and determines that Itachi was weakened before the start of the fight.Eventually overcome by his injuries, Itachi approaches his brother, who can also no longer fight. Itachi taps Sasuke on the forehead before dying of exhaustion, leaving a bloody trail down Sasuke's face.When Sasuke awakens after collapsing, Itachi's true backstory is revealed. He learns that Itachi murdered his clan as a part of his sworn oath to protect their home. The elders of the Uchiha clan were planning a coup, thereby throwing the relatively stable Land of Fire back into a series of small wars. Itachi, who was a child during the last Great War between the nations, realized that turning on his clan was the only way to prevent uncountable deaths. He killed his entire family and was labeled a traitor, but all the while he was working in service to his country.The traitor status served a dual purpose, protecting Sasuke from being viewed with suspicion and allowing Itachi to infiltrate the rogue shinobi organization Akatsuki. He worked as a spy for the Land of Fire, passing on information about their battle plans to leadership and keeping himself alive just long enough so that his brother could come and have his revenge.Sasuke is not the dyed-in-the-wool patriot that his late brother was. Seeing the machinations of his home laid plain, and the way they were willing to sacrifice his entire family for their own well-being, pushes Sasuke to the actual areas that his brother was only engaging in as a double agent.The murder of his clan and his all-consuming quest for revenge had effectively ruined Sasuke's life. He turned his back on his friends and his home, left the Academy before his training was over, fell in with an evil shinobi who nearly destroyed him and ultimately murdered his last living relative, all in the service of a lie. Figuring all of this out led him to vow that he would see his village burned.His new goal in life becomes the the complete and utter destruction of Konoha. He joins the Akatsuki to further his goals.Immediately before the battle between brothers, more is discovered about the organization to which Itachi nominally belongs. In a battle between Naruto's mentor Jiraiya and Akatsuki leader Nagato, the core ideology of the group is revealed: They hope to unleash all of the sealed beasts in one attack (called the Infinite Tsukuyomi) that will seal the entire world in a dream forever.The group is made up of disillusioned Shinobi who, seeing the effects of several great wars between nations, hope to put a permanent end to the concept of war. Jiraiya is killed in the battle before he can share any information about the group and its leadership."
+        
+        
+    def year2(self):
+            print("\n","Creator:Masashi Kishimoto","\n")
+            
+class Story_Of_Dragon_Ball():
+    def year3(self):
+        print("\n","Story Of Dragon Ball","\n")
+        print("Dragon Ball Super is the official sequel to Dragon Ball Z, supplanting Dragon Ball GT and placing it out of continuity. The anime aired from 2015 to 2018, though the manga that it's based on continues to this day. Set ten years after the defeat of Majin Buu, it began with adapting the movies Battle of the Gods and Resurrection of F.")
+        
+    def year4(self):
+            print("\n","Creator:Akira Toriyama","\n")
+            
+class Story_Of_Dragon_Ball():
+    def year5(self):
+        print("\n","Story Of Bleach","\n")
+        print("Bleach (stylized as BLEACH) is a Japanese manga series written and illustrated by Tite Kubo. It follows the adventures of the hotheaded teenager Ichigo Kurosaki, who inherits his parents' destiny after he obtains the powers of a Soul Reaper —a death personification similar to the Grim Reaper —from another Soul Reaper, Rukia Kuchiki.")
+        
+    def year6(self):
+            print("\n","Creator:Tite Kubo","\n")
+            
+            
+Naruto=Story_Of_Naruto()
+Naruto.year1()
+Naruto.year2()
+
+Dragon Ball=Story_Of_Dragon_Ball()
+Dragon Ball.year3()
+Dragon Ball.year4()
+
+sports=Story_Of_Dragon_Ball()
+sports.year5()
+sports.year6()
